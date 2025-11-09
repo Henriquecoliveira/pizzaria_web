5acd8e231d5d3f1839cc71745b02350c2dac5225
Henrique Oliveira <henriquecoliveira365@gmail.com>
2025-11-09 07:11:26 -0300

    base da feature-pedidos

--- ts/server.ts
+++ ts/server.ts
@@ -7,8 +7,6 @@ import bodyParser from 'body-parser'; // converte o corpo das requisições em J
 import cors from 'cors';
 
 // imports locais
-import { cadastrarCliente } from './pagina_login_cadastro'; // função para cadastrar cliente no banco
-import { Cliente } from './models/cliente'; // interface Cliente
 
 const app = express(); // cria a aplicação express
 
@@ -17,28 +15,6 @@ app.use(cors());
 
 // converte o corpo das requisições de strings para JSON
 app.use(bodyParser.json());
-
-// rota para cadastrar usuário
-app.post('/api/usuarios', async (req, res) => {
-
-    const { email, usuario, senha, cpf, telefone } = req.body;
-    // extrai cada campo do corpo da requisição
-
-
-    // monta o objeto do cliente
-    const cliente: Cliente = {
-      email,
-      usuario,
-      senha,
-      cpf,
-      telefone,
-    };
-
-    // cadastra no banco
-    await cadastrarCliente(cliente);
-    res.send("Usuário cadastrado com sucesso!");
-
-}); 
 
 // inicia o servidor
 const PORT = process.env.PORT || 3000; // porta padrao 3000 usada pelo nodeJS em desenvolvimento de servidores web 
